6b2501ff2c11df9c5d5cca68a77493b7757cfb80
leo <leobaranelli@gmail.com>
2024-06-06 14:42:36 -0300

    feat: services crud

--- backend/src/handlers/postService.js
+++ backend/src/handlers/postService.js
@@ -1,21 +1,24 @@
-const postData = [];
+const { _postService } = require("../controllers/_postService");
 
 const postService = async (req, res) => {
+  const { name, apiKey } = req.body;
+
   try {
-    postData.push(req.body);
+    const newService = await _postService(name, apiKey);
+
     res.status(201).json({
-      message: "The data was received successfully",
-      data: req.body,
+      message: "The service instance was created successfully",
+      service: newService,
     });
-  } catch (error) {
+  } 
+  catch (error) {
     res.status(400).json({
       error: true,      
-      message: error.message || "Unknown error",
+      message: error.message || 'Unknown error',
     });
   }
 };
 
 module.exports = {
   postService,
-  postData,
 };
