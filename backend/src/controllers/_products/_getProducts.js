a31749ca17058ca72b739e52747eda3cdb03d577
leo <leobaranelli@gmail.com>
2024-06-08 22:23:13 -0300

    feat: products module (router and CRUD)

---
+++ backend/src/controllers/_products/_getProducts.js
@@ -0,0 +1,15 @@
+const { Product } = require("../../db");
+
+const _getProducts = async () => {  
+  const allProducts = await Product.findAll();
+
+  if(allProducts.length === 0){
+    throw new Error("There are no registered products yet");
+  }else {    
+    return allProducts;    
+  }
+};
+
+module.exports = {
+  _getProducts,
+};
