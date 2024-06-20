451baecc7433df1f99eec0d3a5eb75c647812cda
leo <leobaranelli@gmail.com>
2024-06-20 09:11:27 -0300

    rollback: delete current frontend version

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./main.css";
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
-
-
-createApp(App).use(router).mount("#app");
