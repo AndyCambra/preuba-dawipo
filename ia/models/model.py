5b6f99957533e9c655223eac1a0e69be69147aea
juanpintoselso33 <juanpintoselso33@gmail.com>
2024-06-21 11:25:58 -0300

    DWAI-028: Add all dependencies to requirements.txt and improve instructions in README

--- ia/models/model.py
+++ ia/models/model.py
@@ -1,5 +1,5 @@
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
-model_path = "ia/best-fine-tuned-t5-base"  # Ruta relativa al directorio de trabajo
+model_path = "ia/models/best-fine-tuned-t5-base"  # Ruta relativa al directorio de trabajo
 model = T5ForConditionalGeneration.from_pretrained(model_path)
 tokenizer = T5Tokenizer.from_pretrained(model_path)
