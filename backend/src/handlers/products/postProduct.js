bc8476c6a13e96a50804a242cb25e9125469e1f1
juanpintoselso33 <juanpintoselso33@gmail.com>
2024-06-21 17:58:59 -0300

    DWB-029: Handle case where Flask server is down, use original product data if Flask server is unavailable

--- backend/src/handlers/products/postProduct.js
+++ backend/src/handlers/products/postProduct.js
@@ -10,14 +10,21 @@ const postProduct = async (req, res) => {
     for (const product of products) {
       console.log("Product:", product);
 
-      const response = await axios.post(
-        "http://localhost:5000/add_product",
-        product
-      );
-      const transformedProduct = response.data;
+      let transformedProduct;
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/add_product",
+          product
+        );
+        transformedProduct = response.data;
+        console.log("Transformed Product:", transformedProduct);
+      } catch (flaskError) {
+        console.error(
+          "Flask server is not available, using original product data."
+        );
+        transformedProduct = product; // Use the original product if Flask server is not available
+      }
 
-      console.log("Transformed Product:", transformedProduct);
-      
       const newProduct = await _postProduct({
         name: transformedProduct.name,
         originCountry: transformedProduct.originCountry,
