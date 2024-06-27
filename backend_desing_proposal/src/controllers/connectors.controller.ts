5b98026700430397463d6ff56c2561db4bb05d8b
leo <leobaranelli@gmail.com>
2024-06-26 22:23:35 -0300

    feat(new_backend): connectors module

--- backend_desing_proposal/src/controllers/connectors.controller.ts
+++ backend_desing_proposal/src/controllers/connectors.controller.ts
@@ -1,15 +1,68 @@
 import { Request, Response } from 'express';
-import { Connector } from '../models/connector.model';
+import ConnectorsService from '../services/connectors.service';
 import { handleControllerError } from '../utils/error.handlers';
+import { ConnectorAttributes } from '../types/models.interfaces';
 
 export default class ConnectorsController {
 
   public static getAll = async (_req: Request, res: Response) => {
-    try {            
-      const connectors = await Connector.findAll();
-      res.json(connectors);
-    } catch (error: any) {      
+    try {
+      const allConnectors = await ConnectorsService.getAllConnectors();
+      res.json(allConnectors);
+    } catch (error: any) {
       handleControllerError(error, res);
     }
-  }; 
+  };
+
+  public static getByName = async (req: Request, res: Response) => {
+    const { name } = req.params;
+    try {
+      const connector = await ConnectorsService.getConnectorByName(name);
+      res.json(connector);
+    } catch (error: any) {
+      handleControllerError(error, res);
+    }
+  };
+
+  public static getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const connector = await ConnectorsService.getConnectorById(id);
+      res.json(connector);
+    } catch (error: any) {
+      handleControllerError(error, res);
+    }
+  };
+
+  public static create = async (req: Request, res: Response) => {
+    const connectorData: ConnectorAttributes = req.body;
+    try {
+      const newConnector = await ConnectorsService.createConnector(connectorData);
+      res.json(newConnector);
+    } catch (error: any) {
+      handleControllerError(error, res);
+    }
+  };
+
+  public static updateByName = async (req: Request, res: Response) => {
+    const { name } = req.params;
+    const updates: Partial<ConnectorAttributes> = req.body;
+   
+    try {
+      const updatedConnector = await ConnectorsService.updateConnectorByName(name, updates);
+      res.json(updatedConnector);
+    } catch (error: any) {
+      handleControllerError(error, res);
+    }
+  };
+  
+  public static deleteByName = async (req: Request, res: Response) => {
+    const { name } = req.params;
+    try {
+      const result = await ConnectorsService.deleteConnectorByName(name);
+      res.json(result);
+    } catch (error: any) {
+      handleControllerError(error, res);
+    }
+  };
 }
